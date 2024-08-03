d1aa6a728e3e55814eeb1bf5e3614698677003ff
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-08-03 17:29:44 +0300

    Create projects

---
+++ src/Client/CompressionService/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CompressionService
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/Client/CompressionServiceTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CompressionServiceTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
